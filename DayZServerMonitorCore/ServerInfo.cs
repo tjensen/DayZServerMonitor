0ed9244b746173ff5f4b438354974b333df27f94
Tim Jensen <tim.l.jensen@gmail.com>
2019-07-04 15:16:52 -0500

    Added ServerInfo class for holding parsed information retrieved from a server.

---
+++ DayZServerMonitorCore/ServerInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace DayZServerMonitorCore
+{
+    public class ServerInfo
+    {
+        private readonly string address;
+        private readonly string name;
+        private readonly int numPlayers;
+        private readonly int maxPlayers;
+
+        public ServerInfo(string address, string name, int numPlayers, int maxPlayers)
+        {
+            this.address = address;
+            this.name = name;
+            this.numPlayers = numPlayers;
+            this.maxPlayers = maxPlayers;
+        }
+
+        public string Address { get => address; }
+        public string Name { get => name; }
+        public int NumPlayers { get => numPlayers; }
+        public int MaxPlayers { get => maxPlayers; }
+    }
+}
---
+++ TestDayZServerMonitorCore/TestServerInfo.cs
@@ -0,0 +1,35 @@
+﻿using DayZServerMonitorCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDayZServerMonitorCore
+{
+    [TestClass]
+    public class TestServerInfo
+    {
+        private readonly ServerInfo info = new ServerInfo("address", "name", 42, 60);
+
+        [TestMethod]
+        public void AddressContainsTheServerAddress()
+        {
+            Assert.AreEqual("address", info.Address);
+        }
+
+        [TestMethod]
+        public void NameContainsTheServerName()
+        {
+            Assert.AreEqual("name", info.Name);
+        }
+
+        [TestMethod]
+        public void NumPlayersContainsTheNumberOfPlayers()
+        {
+            Assert.AreEqual(42, info.NumPlayers);
+        }
+
+        [TestMethod]
+        public void MaxPlayersContainsTheMaximumNumberOfPlayers()
+        {
+            Assert.AreEqual(60, info.MaxPlayers);
+        }
+    }
+}
