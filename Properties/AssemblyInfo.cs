b663cd7aa4b48f687aa462f079a75bc037024fa4
Tim Jensen <tim.l.jensen@gmail.com>
2019-07-07 16:32:34 -0500

    Preparing for first release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DayZServerMonitor")]
+[assembly: AssemblyTitle("DayZ Server Monitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DayZServerMonitor")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("DayZ Server Monitor")]
+[assembly: AssemblyCopyright("Copyright © Tim Jensen 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
