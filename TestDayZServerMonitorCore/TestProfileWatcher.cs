4e994e79ead814c8f606429c64d4555e014399f0
Tim Jensen <tim.l.jensen@gmail.com>
2019-07-15 19:51:07 -0500

    Added error handling to ProfileWatcher so that the program can continue to run when the DayZ profile folder doesn't exist.

--- TestDayZServerMonitorCore/TestProfileWatcher.cs
+++ TestDayZServerMonitorCore/TestProfileWatcher.cs
@@ -9,18 +9,30 @@ namespace TestDayZServerMonitorCore
     [TestClass]
     public class TestProfileWatcher
     {
+        private string tempdir;
         private string filename;
 
         [TestInitialize]
         public void Initialize()
         {
-            filename = Path.GetTempFileName();
+            tempdir = Path.GetTempFileName();
+            File.Delete(tempdir);
+            Directory.CreateDirectory(tempdir);
+            filename = Path.Combine(tempdir, "some-file.tmp");
+            File.WriteAllText(filename, "initial-content");
         }
 
         [TestCleanup]
         public void Cleanup()
         {
-            File.Delete(filename);
+            try
+            {
+                Directory.Delete(tempdir, true);
+            }
+            catch (DirectoryNotFoundException error)
+            {
+                Console.WriteLine("Unable to delete {0}: {1}", tempdir, error);
+            }
         }
 
         [TestMethod]
@@ -40,6 +52,24 @@ namespace TestDayZServerMonitorCore
                 File.AppendAllText(filename, "some-data");
 
                 Assert.IsTrue(finished.WaitOne(50, false));
+            }
+        }
+
+        [TestMethod]
+        public void DoesNothingIfDirectoryDoesNotExist()
+        {
+            AutoResetEvent finished = new AutoResetEvent(false);
+            Action action = new Action(() => finished.Set());
+
+            Directory.Delete(tempdir, true);
+
+            using (ProfileWatcher watcher = new ProfileWatcher(
+                Path.GetDirectoryName(filename),
+                Path.GetFileName(filename),
+                null,
+                action))
+            {
+                Assert.IsTrue(true);
             }
         }
 
