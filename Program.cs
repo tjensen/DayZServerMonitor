945e7e39a2c4c57124b275078fda54b097885e1b
Tim Jensen <tim.l.jensen@gmail.com>
2019-08-31 15:11:27 -0500

    Removing unnecessary using statements

--- Program.cs
+++ Program.cs
@@ -1,18 +1,11 @@
-﻿using DayZServerMonitorCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DayZServerMonitor
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
