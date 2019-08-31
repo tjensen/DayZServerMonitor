8123c37f854088d77349c987e4fc37ef3f37b703
Tim Jensen <tim.l.jensen@gmail.com>
2019-08-31 10:38:21 -0500

    Adding DynamicIcons class for dynamically generating icons suitable for the system tray that show the current number of players.

---
+++ DayZServerMonitorCore/DynamicIcons.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace DayZServerMonitorCore
+{
+    public class DynamicIcons : IDisposable
+    {
+        // Row positions
+        private static readonly int TOP = 1;
+        private static readonly int MIDDLE = 7;
+        private static readonly int BOTTOM = 13;
+
+        // Column positions (narrow)
+        private static readonly int LEFT = 1;
+        private static readonly int RIGHT = 4;
+
+        // Column adjustments for character width
+        private static readonly int NARROW = 0;
+        private static readonly int WIDE = 2;
+
+        // Simulates a 7-segment display, where bits 0-6 represent whether to light the
+        // corresponding segment
+        private static readonly int[] DIGITS = {
+            0x7D, 0x50, 0x37, 0x57, 0x5A, 0x4F, 0x6F, 0x51, 0x7F, 0x5F };
+        private static readonly Tuple<Point, Point>[] SEGMENTS =
+        {
+            new Tuple<Point, Point>(new Point(LEFT, TOP), new Point(RIGHT, TOP)),
+            new Tuple<Point, Point>(new Point(LEFT, MIDDLE), new Point(RIGHT, MIDDLE)),
+            new Tuple<Point, Point>(new Point(LEFT, BOTTOM), new Point (RIGHT, BOTTOM)),
+            new Tuple<Point, Point>(new Point(LEFT, TOP), new Point (LEFT, MIDDLE)),
+            new Tuple<Point, Point>(new Point(RIGHT, TOP), new Point (RIGHT, MIDDLE)),
+            new Tuple<Point, Point>(new Point(LEFT, MIDDLE), new Point (LEFT, BOTTOM)),
+            new Tuple<Point, Point>(new Point(RIGHT, MIDDLE), new Point (RIGHT, BOTTOM))
+        };
+
+        private readonly Dictionary<uint, Icon> numberIcons = new Dictionary<uint, Icon>();
+        private Icon unknownIcon = null;
+
+        public Icon GetIconForNumber(uint value)
+        {
+            if (!numberIcons.ContainsKey(value))
+            {
+                using (Bitmap bmp = new Bitmap(16, 16))
+                {
+                    using (Graphics graph = Graphics.FromImage(bmp))
+                    {
+                        graph.FillRectangle(Brushes.Black, 0, 0, 16, 16);
+                        DrawNumber(graph, value, Brushes.White);
+                        numberIcons[value] = Icon.FromHandle(bmp.GetHicon());
+                    }
+                }
+            }
+            return numberIcons[value];
+        }
+
+        public Icon GetIconForUnknown()
+        {
+            if (unknownIcon == null)
+            {
+                using (Bitmap bmp = new Bitmap(16, 16))
+                {
+                    using (Graphics graph = Graphics.FromImage(bmp))
+                    {
+                        graph.FillRectangle(Brushes.Black, 0, 0, 16, 16);
+                        DrawX(graph, Brushes.White);
+                    }
+                    unknownIcon = Icon.FromHandle(bmp.GetHicon());
+                }
+            }
+            return unknownIcon;
+        }
+
+        private Point AdjustX(Point point, int widthAdjust, int left)
+        {
+            // Returns a new point after adjusting for the left column and the character width
+            Point result = new Point(point.X, point.Y);
+            result.Offset(left + ((result.X == RIGHT) ? widthAdjust : 0), 0);
+            return result;
+        }
+
+        private void DrawDigit(Graphics graph, int left, uint digit, int widthAdjust, Pen pen)
+        {
+            // Draws a single digit to look like a 7-segment display
+            for (int segment = 0; segment < 7; segment++)
+            {
+                if ((DIGITS[digit] & (1 << segment)) != 0)
+                {
+                    graph.DrawLine(
+                        pen,
+                        AdjustX(SEGMENTS[segment].Item1, widthAdjust, left),
+                        AdjustX(SEGMENTS[segment].Item2, widthAdjust, left));
+                }
+            }
+        }
+
+        private void DrawNumber(Graphics graph, uint value, Brush brush)
+        {
+            using (Pen pen = new Pen(brush))
+            {
+                if (value < 10)
+                {
+                    DrawDigit(graph, 4, value, WIDE, pen);
+                }
+                else if (value < 100)
+                {
+                    DrawDigit(graph, 0, (value / 10) % 10, WIDE, pen);
+                    DrawDigit(graph, 8, value % 10, WIDE, pen);
+                }
+                else
+                {
+                    // This really only works for 100...199 since the third digit only has enough
+                    // room for a one.
+                    DrawDigit(graph, -2, (value / 100) % 10, NARROW, pen);
+                    DrawDigit(graph, 4, (value / 10) % 10, NARROW, pen);
+                    DrawDigit(graph, 10, value % 10, NARROW, pen);
+                }
+            }
+        }
+
+        private void DrawX(Graphics graph, Brush brush)
+        {
+            using (Pen pen = new Pen(brush))
+            {
+                const int left = 4;
+                graph.DrawLine(
+                    new Pen(brush),
+                    AdjustX(new Point(LEFT, TOP), WIDE, left),
+                    AdjustX(new Point(RIGHT, BOTTOM), WIDE, left));
+                graph.DrawLine(
+                    new Pen(brush),
+                    AdjustX(new Point(RIGHT, TOP), WIDE, left),
+                    AdjustX(new Point(LEFT, BOTTOM), WIDE, left));
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    foreach (var entry in numberIcons)
+                    {
+                        entry.Value.Dispose();
+                    }
+                    if (unknownIcon != null)
+                    {
+                        unknownIcon.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ TestDayZServerMonitorCore/TestDynamicIcons.cs
@@ -0,0 +1,151 @@
+﻿using DayZServerMonitorCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace TestDayZServerMonitorCore
+{
+    [TestClass]
+    public class TestDynamicIcons
+    {
+        private ushort GetRowFromBitmap(Bitmap bmp, int row)
+        {
+            ushort result = 0;
+            for (int column = 0; column < 16; column++)
+            {
+                Color pixel = bmp.GetPixel(column, row);
+                if (pixel.ToArgb() == Color.White.ToArgb())
+                {
+                    result |= (ushort)(1 << column);
+                }
+                else if (pixel.ToArgb() != Color.Black.ToArgb())
+                {
+                    Assert.Fail($"Invalid pixel color {pixel} at column {column}, row {row}");
+                }
+            }
+            return result;
+        }
+
+        private void AssertIconHasPixels(ushort[] rows, Icon icon)
+        {
+            using (Bitmap bmp = icon.ToBitmap())
+            {
+                for (int row = 0; row < 16; row++)
+                {
+                    ushort actual = GetRowFromBitmap(bmp, row);
+                    Assert.AreEqual(
+                        rows[row],
+                        actual,
+                        $"Row {row} pixels (0x{actual:X4}) do not match expected (0x{rows[row]:X4})");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForNumberReturnsIconShowingSingleDigit()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon = icons.GetIconForNumber(8);
+
+                Assert.AreEqual(16, icon.Width);
+                Assert.AreEqual(16, icon.Height);
+                AssertIconHasPixels(new ushort[16]
+                    {
+                        0x0000, 0x07E0, 0x0420, 0x0420, 0x0420, 0x0420, 0x0420, 0x07E0,
+                        0x0420, 0x0420, 0x0420, 0x0420, 0x0420, 0x07E0, 0x0000, 0x0000
+                    },
+                    icon);
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForNumberReturnsIconShowingTwoDigits()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon = icons.GetIconForNumber(88);
+
+                Assert.AreEqual(16, icon.Width);
+                Assert.AreEqual(16, icon.Height);
+                AssertIconHasPixels(new ushort[16]
+                    {
+                        0x0000, 0x7E7E, 0x4242, 0x4242, 0x4242, 0x4242, 0x4242, 0x7E7E,
+                        0x4242, 0x4242, 0x4242, 0x4242, 0x4242, 0x7E7E, 0x0000, 0x0000
+                    },
+                    icon);
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForNumberReturnsIconShowingThreeDigits()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon = icons.GetIconForNumber(188);
+
+                Assert.AreEqual(16, icon.Width);
+                Assert.AreEqual(16, icon.Height);
+                AssertIconHasPixels(new ushort[16]
+                    {
+                        0x0000, 0x79E4, 0x4924, 0x4924, 0x4924, 0x4924, 0x4924, 0x79E4,
+                        0x4924, 0x4924, 0x4924, 0x4924, 0x4924, 0x79E4, 0x0000, 0x0000
+                    },
+                    icon);
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForNumberReturnsSameIconForSameInput()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon1 = icons.GetIconForNumber(42);
+                Icon icon2 = icons.GetIconForNumber(42);
+
+                Assert.AreSame(icon1, icon2);
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForNumberReturnsDifferentIconsForDifferentInputs()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon1 = icons.GetIconForNumber(42);
+                Icon icon2 = icons.GetIconForNumber(24);
+
+                Assert.AreNotSame(icon1, icon2);
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForUnknownReturnsIconShowingSingleX()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon = icons.GetIconForUnknown();
+
+                Assert.AreEqual(16, icon.Width);
+                Assert.AreEqual(16, icon.Height);
+                AssertIconHasPixels(new ushort[16]
+                    {
+                        0x0000, 0x0420, 0x0420, 0x0240, 0x0240, 0x0180, 0x0180, 0x0180,
+                        0x0180, 0x0180, 0x0240, 0x0240, 0x0420, 0x0420, 0x0000, 0x0000
+                    },
+                    icon);
+            }
+        }
+
+        [TestMethod]
+        public void GetIconForUnknownReturnsSameIconEachTime()
+        {
+            using (DynamicIcons icons = new DynamicIcons())
+            {
+                Icon icon1 = icons.GetIconForUnknown();
+                Icon icon2 = icons.GetIconForUnknown();
+
+                Assert.AreSame(icon1, icon2);
+            }
+        }
+    }
+}
